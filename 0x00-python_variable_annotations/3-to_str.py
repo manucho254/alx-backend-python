bd5d63437a616c453ffc97fd25524fb777811143
manucho254 <manuchoadero@gmail.com>
2024-01-11 13:38:16 +0000

    function to return flaot as a string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" module to return string representation of a float
+"""
+
+
+def to_str(n: float) -> str:
+    """ convert float to string
+        Args:
+            n: float value
+        Return:
+            n as a tring
+    """
+    return "{}".format(n)
