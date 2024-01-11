5a117136880f4eb38e52299548210d0ccfc27117
manucho254 <manuchoadero@gmail.com>
2024-01-11 13:29:03 +0000

    function to get floor of a float

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" module to get floor of a float
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """ get floor of n
+        Args:
+            n: float value
+        Return:
+            floor of n
+    """
+    return math.floor(n)
