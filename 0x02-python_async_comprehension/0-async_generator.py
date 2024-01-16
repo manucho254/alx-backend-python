48208922ace047887b18455a453194504f786dae
manucho254 <manuchoadero@gmail.com>
2024-01-16 12:55:47 +0000

    Async Generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" Async Generator module
+"""
+import asyncio
+import random
+from typing import Generator
+
+
+async def async_generator() -> Generator[int, None, None]:
+    """ Async Generator function
+        Return:
+              a generator
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.randint(0, 10)
