b8a4f75456ec39905378cb6b4e94a0dd1b34123f
Emmanuel manucho <manuchoadero@gmail.com>
2024-02-04 18:37:10 +0300

    Parameterize a unit test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -31,3 +31,14 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, expected):
         """test access nested map function"""
         self.assertEqual(utils.access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand(
+        [
+            ({}, ("a",), KeyError),
+            ({"a": 1}, ("a", "b"), KeyError),
+        ]
+    )
+    def test_access_nested_map_exception(self, nested_map, path, expected):
+        """test access nested map invalid key"""
+        with self.assertRaises(expected) as er:
+            utils.access_nested_map(nested_map, path)
