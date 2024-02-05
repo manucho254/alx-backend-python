b155349de0a5cc3cb266283896e59e3efa2608ec
manucho254 <manuchoadero@gmail.com>
2024-02-05 13:29:26 +0000

    fixes

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -36,7 +36,8 @@ class TestGithubOrgClient(unittest.TestCase):
             return_value=TEST_DATA, new_callable=PropertyMock
         ) as mock_public_repos_url:
             client = GithubOrgClient("google")
-            self.assertEqual(client._public_repos_url, TEST_DATA.get("repos_url"))
+            self.assertEqual(client._public_repos_url,
+                             TEST_DATA.get("repos_url"))
 
     @mock.patch.object(
         GithubOrgClient,
